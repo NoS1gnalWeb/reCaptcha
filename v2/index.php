579dbf63ba36372f58681b8715caae0cf3def4ea
Geistnigmareicharbeitungerheit <37757623+NoS1gnalWeb@users.noreply.github.com>
2021-03-21 13:19:03 +0100

    Update index.php

--- v2/index.php
+++ v2/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>reCATPCHA V3</title>
+    <title>reCATPCHA V2</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
